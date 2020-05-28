77b2e5b8411b92864a6ba6aa090e6603394b2294
Eric Sheppard <eric@eigenmo.de>
2020-05-28 13:43:16 +1000

    v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="networkbilling-EIGENMODE",
-    version="0.0.1",
+    version="0.0.2",
     author="Eric Sheppard",
     author_email="eric@eigenmo.de",
     description="Parse and Verify Network Bill Files",
