92764bb50f3316ab08cdbb47e0c3798897c24a8d
Eric Sheppard <eric@sheppard.cloud>
2021-09-16 06:31:31 +0000

    v0.1.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="networkbilling-EIGENMODE",
-    version="0.1.11",
+    version="0.1.12",
     author="Eric Sheppard",
     author_email="eric@eigenmo.de",
     description="Parse and Verify Network Bill Files",
